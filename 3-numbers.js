0687b77e9f042ab9e4e7541d03825b8f2df6f508
Nicolas <franconicolas2512@gmail.com>
2022-07-08 21:01:29 -0300

    Numbers

---
+++ 3-numbers.js
@@ -0,0 +1,10 @@
+//! NUMBERS --> Vamos a utilizar el objeto Math
+
+//* REDONDEO Y TRUNCAMIENTO
+
+// 35.73
+// 36 <-- REDONDEO
+// 35 <-- TRUNCAMIENTO
+
+let numberOne = 35.73;
+console.log('Número redondeado:' Math.round(numberOne));
